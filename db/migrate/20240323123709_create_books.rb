f32ed60eea7cbd37851a7cce38d560db2305b39f
EC2 Default User <ec2-user@ip-172-31-3-209.ap-northeast-1.compute.internal>
2024-03-28 12:49:53 +0000

    Fixed

--- db/migrate/20240323123709_create_books.rb
+++ db/migrate/20240323123709_create_books.rb
@@ -3,7 +3,7 @@ class CreateBooks < ActiveRecord::Migration[6.1]
     create_table :books do |t|
       t.string :title
       t.string :body
-      t.references :user, foreign_key: true  # user_idという名前の整数型のカラムを追加する
+      t.integer :user_id
       t.timestamps
     end
   end
